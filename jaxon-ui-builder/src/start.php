32529382ac74bc4a7abed5b89237997d623891b1
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2024-07-30 06:30:31 +0200

    Register the UI builder in the DI container.

--- jaxon-ui-builder/src/start.php
+++ jaxon-ui-builder/src/start.php
@@ -10,8 +10,9 @@ use function Jaxon\jaxon;
 function initBuilder()
 {
     $di = jaxon()->di();
+    // Register the Jxon tag builder.
     $di->auto(JaxonTagBuilder::class);
-    // Register the UI builder setup
+    // Register the UI builder setup.
     $di->set(BuilderSetup::class, function($di) {
         $jaxonTagBuilder = $di->g(JaxonTagBuilder::class);
         $setup = new BuilderSetup();
@@ -20,6 +21,13 @@ function initBuilder()
             $jaxonTagBuilder->tag($builder, $method, $arguments);
         });
         return $setup;
+    });
+    // Register the UI builder.
+    $di->set(BuilderInterface::class, function($di) {
+        $app = $di->getApp();
+        // The 'ui_builder_<name>' key is defined by the corresponding template package.
+        return !$app->hasAppOption('ui.template') ? null :
+            $di->g('ui_builder_' . $app->getAppOption('ui.template'));
     });
 }
 
